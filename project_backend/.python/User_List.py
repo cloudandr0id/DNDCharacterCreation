fc5b6e16ca1f43fdd62a51b705ded3fdc2ad6c77
cloudandr0id <57876679+cloudandr0id@users.noreply.github.com>
2021-03-08 18:57:32 -0700

    Update User_List.py

--- project_backend/.python/User_List.py
+++ project_backend/.python/User_List.py
@@ -1,1 +1,27 @@
-s
+class userList:
+    accountFile = "accountsList.txt"
+
+    def __init__(self, account):
+        self.account = account
+
+    # imports a new/updated account into the accountsList.txt file
+    def import():
+        fileObject = open(accountFile, "a")
+        fileObject.write(accountFile, "")
+        fileObject.write('\n')
+        fileObject.close()
+
+    # displays list for admin purposes only
+    def displayUserList():
+        # goes through each line and displays accountslist
+        pass
+
+    # searches for a user in the list
+    # returns boolean based on if user exists in list or not
+    def searchUser(email):
+        pass
+
+    def login():
+        # goes in and searches for an account based on username
+        # checks the password associated with the account and one given match
+        pass
