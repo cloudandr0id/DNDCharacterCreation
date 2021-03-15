c97a8b9ff9c2a8eb861492fc4e62092da3f4c1e8
rmccormick314 <rmccormick314@gmail.com>
2021-03-14 19:38:35 -0700

    cancercode

---
+++ project_frontend/public/characterCreator/creator_script.js
@@ -0,0 +1,14 @@
+<script>
+  function webpage()
+  {
+    document.write("<html>");
+    document.write("<head> <title>Temporary Web Page</title> </head>");
+    document.write("<body bgcolor= \"white\">");
+
+    document.write("<p>STR: ") + ;
+    document.write("<p>Everything on this page is written with html");
+    document.write("<p>I can even put buttons on this page");
+    document.write("<p><input type=\"button\" value=\"Click Me\">");
+    document.write("</body> </html>");
+  }
+</script>
