cc0c174f486d325e4dd81efebcdd9f838392b1c5
horkoborko <47957160+horkoborko@users.noreply.github.com>
2021-03-16 15:31:26 -0700

    merge
    merging server content to github

---
+++ project_frontend/public/header.php
@@ -0,0 +1,25 @@
+<?php
+echo '<img id="logoimage" alt="DND CC Logo" src="/.buttons/logo.png">
+
+<a href="/">
+	<img id="headerButton" alt="Home" src="/.buttons/HomeButton.png">
+</a>
+
+<a	href="characters/">
+	<img id="headerButton" alt="Characters" src="/.buttons/CharButton.png">
+</a>
+
+<a href= "contact/">
+  <img id="headerButton" alt="About/Contact" src="/.buttons/ContButton.png">
+</a>
+
+<a href="https://github.com/cloudandr0id/DNDCharacterCreation" target="_blank">
+  <img id="headerButton" alt="Github" src="/.buttons/GHButton.png">
+</a>
+
+<a href="profile/">
+  <img id="headerButton" alt="Profile" src="/.buttons/AccButton.png">
+</a>
+
+<img id="headerButton" alt="Login" src="/.buttons/LogButton.png">';
+?>
