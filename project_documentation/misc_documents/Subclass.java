60e46d3e8772e77030625b14e7f5aa59f99e1cef
Jonathan <j.nation99@yahoo.com>
2021-03-27 15:07:14 -0700

    Create Subclass.java

---
+++ project_documentation/misc_documents/Subclass.java
@@ -0,0 +1,25 @@
+public class Subclass extends Class
+{
+   private int classID;   
+   private String className;
+
+   public int getClassID()
+     {
+       return classID;
+     }
+
+   public void setClassID(int id)
+     {
+       classID = id;
+     }
+
+   public void setClassName(String name)
+     {
+       className = name;
+     }
+
+   public String getClassName()
+     {
+       return className;
+     }
+}
