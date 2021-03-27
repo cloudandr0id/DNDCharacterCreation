299539d32f7aa7b334da957a006d6e203648af7b
Jonathan <j.nation99@yahoo.com>
2021-03-27 16:08:06 -0700

    Create Ability.java

---
+++ project_documentation/misc_documents/Ability.java
@@ -0,0 +1,18 @@
+public class AbliltyScore
+{
+   private int scoreID;   
+   private String scoreName;
+   private int score;
+   private int modifier;
+
+
+   public int calculateScore()
+     {
+	       
+     }
+   
+   public int scoreToModifier()
+     {
+	
+     }
+}
