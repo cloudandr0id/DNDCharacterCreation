eb9c9ef0620f91581a4d915560f7ee58fb4f9022
Jonathan <j.nation99@yahoo.com>
2021-03-27 15:44:32 -0700

    Create RaceTraits.java

---
+++ project_documentation/misc_documents/RaceTraits.java
@@ -0,0 +1,25 @@
+public class RaceTraits extends Race
+{
+   private int raceSpeed;
+   private int raceTrait;
+
+   public void setSpeed(int speed)
+     {
+       raceSpeed = speed;
+     }
+
+   public void getSpeed()
+     {
+       return raceSpeed;
+     }
+
+   public int setTrait(int trait)
+     {
+       raceTrait = trait;
+     }
+
+   public int getTrait()
+     {
+       return raceTrait;
+     }
+}
