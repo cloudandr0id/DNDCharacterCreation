7f759af612d03c8d22b95c60ee7a30e65cfc06aa
Jonathan <j.nation99@yahoo.com>
2021-03-27 14:54:10 -0700

    Create Class.java

---
+++ project_documentation/misc_documents/Class.java
@@ -0,0 +1,34 @@
+public class Class{
+   private int classID;   
+   private String className;
+   private int classTrait;
+
+   public int getClassID()
+     {
+       return classID;
+     }
+
+   public void setClassID(int id)
+     {
+       classID = id;
+     }
+
+   public void setClassName(String name)
+     {
+       className = name;
+     }
+
+   public String getClassName()
+     {
+       return className;
+     }
+
+   public int setTrait(int trait)
+     {
+       classtrait = trait;
+     }
+
+   public int getTrait()
+     {
+       return classTrait;
+     }
