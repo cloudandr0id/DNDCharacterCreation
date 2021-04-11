0c956cba5dbd9f056988f00a92e8909594d1d9b6
cloudandr0id <57876679+cloudandr0id@users.noreply.github.com>
2021-04-11 00:35:55 -0700

    Update server.js

--- project_frontend/server.js
+++ project_frontend/server.js
@@ -28,3 +28,61 @@ if (process.argv[2] === 'local') {
 }
 
 server.listen(port, () => console.log('Ready on localhost!'))
+
+server.post('/characterCreator/index.html', (req, res) =>{
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+          <head>
+            <link rel = "stylesheet" href = "/.css/dndstyle.css">
+            <title>Character Sheet</title>
+            <nav>
+              <img id="logoimage" alt="DND CC Logo" src="/.buttons/logo.png">
+
+              <a href="/">
+                <img id="headerButton" alt="Home" src="/.buttons/HomeButton.png">
+              </a>
+
+              <a	href="/characters/">
+                <img id="headerButton" alt="Characters" src="/.buttons/CharButton.png">
+              </a>
+
+              <a href= "/contact/">
+                <img id="headerButton" alt="About/Contact" src="/.buttons/ContButton.png">
+              </a>
+
+              <a href="https://github.com/cloudandr0id/DNDCharacterCreation" target="_blank">
+                <img id="headerButton" alt="Github" src="/.buttons/GHButton.png">
+              </a>
+
+              <a href="/account/">
+                <img id="headerButton" alt="Account" src="/.buttons/AccButton.png">
+              </a>
+
+              <a href = "/login/">
+                <img id="headerButton" alt="Login" src="/.buttons/LogButton.png">
+              </a>
+              <hr>
+            </nav>
+          </head>
+          <body>
+            <form id="charSheet">
+              <div id="nameBox">
+                <img id="scrollImg" src="/.buttons/NameScroll.png">
+                <p>Name: NAME_HERE</p>
+                <p>Race: RACE_HERE</p>
+                <p>Level: LEVEL_HERE</p>
+                <p>Strength: STR_STAT</p>
+                <p>Dexterity: DEX_STAT</p>
+                <p>Constitution: CON_STAT</p>
+                <p>Wisdom: WIS_STAT</p>
+                <p>Intelligence: INT_STAT</p>
+                <p>Charisma: CHAR_STAT</p>
+                <p>HP: HP_STAT</p>
+              </div>
+             </form>
+          </body>
+        </html>
+        `)
+    console.log('Form complete')
+})
