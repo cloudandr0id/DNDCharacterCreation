ace59d2c73db91865ef69aab94ef7ece340a0ab7
Jonathan <j.nation99@yahoo.com>
2021-04-17 15:25:33 -0700

    Update LevelDict.java

--- project_backend/database classes/LevelDict.java
+++ project_backend/database classes/LevelDict.java
@@ -1,9 +1,22 @@
 package database_classes;
 
-public class LevelDict{
+public class LevelDict
+{
 	public int[] keys;
 	public int[] values;
-	
+
+	public LevelDict()
+	{
+		keys = null;
+		values = null;
+	}
+
+	public LevelDict(int[] keys, int[] values)
+	{
+		this.keys = keys;
+		this.values = values;
+	}
+
 	public void setProficiency(int val){
 		proficiency = val;
 	}
