8a4b1f842cb6676a2543405bdee65fe219a582d5
cloudandr0id <57876679+cloudandr0id@users.noreply.github.com>
2021-04-16 18:56:56 -0700

    Moving around unit tests

---
+++ project_backend/tests/unitTests/AccountMockTest.java
@@ -0,0 +1,22 @@
+package database_classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+public class AccountMockTest
+{
+    @Mock
+    Account account1 = new Account("email@gmail.com", "password123");
+
+    @Test
+    public void testAccountEmail()
+    {
+        String newEmail = "newEmail@gmail.com"
+        account1.setEmail(newEmail);
+        assertEquals(account1.email.getEmail(), newEmail);
+    }
+
+}
---
+++ project_backend/tests/unitTests/CharacterUnitTests.java
@@ -0,0 +1,12 @@
+package database_classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+public class CharacterUnitTests
+{
+    Character character =
+}
