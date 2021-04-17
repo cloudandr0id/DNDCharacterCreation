73d194aed3dc7c63dbbe1bad0e02f9693f30caa8
cloudandr0id <57876679+cloudandr0id@users.noreply.github.com>
2021-04-16 19:13:05 -0700

    created folder for integration tests

---
+++ project_backend/tests/integrationTests/CharacterIntegrationTests.java
@@ -0,0 +1,26 @@
+package database_classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+public class CharacterUnitTests
+{
+    Character character = new Character(None, None, None, None, None, None,
+    None, None);
+
+    @Test
+    public void evaluateName()
+    {
+        character.setName("Billy Bob");
+        assertEquals(account.getName(), "Billy Bob");
+    }
+
+    @Test
+    public void evaluateRace()
+    {
+        [insert mock class]
+    }
+}
