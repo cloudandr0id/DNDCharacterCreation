e57adbe6a53cecdcb1ff98c02a8c5af609d65911
Jonathan <j.nation99@yahoo.com>
2021-04-16 19:42:58 -0700

    Update Background.java

--- project_backend/database classes/Background.java
+++ project_backend/database classes/Background.java
@@ -1,7 +1,22 @@
 package database_classes;
 
-public class Background{
-	public int backgroundID;
-	public String title;
-	public String description;
+public class Background
+{
+  public int backgroundID;
+  public String title;
+  public String description;
+
+  public Background()
+  {
+    backgroundID = 0;
+    title = "";
+    description = "";
+  }
+
+  public Background(int backgroundID, String title, String description)
+  {
+    this.backgroundID = backgroundID;
+    this.title = title;
+    this.description = description;
+  }
 }
