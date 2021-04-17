7921182d16628a030436be7048b6034f83309790
Jonathan <j.nation99@yahoo.com>
2021-04-17 15:25:43 -0700

    Create RaceTest.java

---
+++ project_backend/tests/unitTests/RaceTest.java
@@ -0,0 +1,41 @@
+package database_classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+public class RaceTest
+{
+    Race race = new Race();
+
+    @Test
+    public void evaluateRaceID()
+    {
+        race.setRaceID(1);
+        assertEquals(race.getRaceID, 1);
+    }
+
+    @Test
+    public void evaluateRaceName()
+    {
+        race.setRaceName("Human");
+        assertEquals(race.getRaceName, "Human");
+    }
+
+    @Test
+    public void evaluateRaceTrait()
+    {
+        race.setRaceTrait(2);
+        assertEquals(race.getRaceTrait, 2);
+    }
+
+    @Test
+    public void evaluateRaceSpeed()
+    {
+        race.setRaceTrait(25);
+        assertEquals(race.getRaceTrait, 25);
+    }
+
+}
