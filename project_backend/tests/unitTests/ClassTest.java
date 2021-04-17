9158379a49039f67dfbee18f9a3b416c8ba89622
Jonathan <j.nation99@yahoo.com>
2021-04-17 15:25:36 -0700

    Create ClassTest.java

---
+++ project_backend/tests/unitTests/ClassTest.java
@@ -0,0 +1,34 @@
+package database_classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+public class ClassTest
+{
+    Class class = new Class();
+
+    @Test
+    public void evaluateClassID()
+    {
+        class.setClassID(10);
+        assertEquals(class.getClassID, 10);
+    }
+
+    @Test
+    public void evaluateClassName()
+    {
+        class.setClassName("Wizard");
+        assertEquals(class.getClassName, "Wizard");
+    }
+
+    @Test
+    public void evaluateClassTrait()
+    {
+        class.setClassTrait(2);
+        assertEquals(class.getClassTrait, 2);
+    }
+
+}
